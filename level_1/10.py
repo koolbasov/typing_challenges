456355a5a6cbb13f94bd405d9735b04e54cce382
koolbasov <kolbasov.anton@gmail.com>
2024-09-17 12:04:18 +0300

    level_1 is done

--- level_1/10.py
+++ level_1/10.py
@@ -1,12 +1,12 @@
 from constants import ___
 
 
-def stringify(value: ___) -> ___:
+def stringify(value: str | int | float | None) -> str:
     pass
 
 
 if __name__ == "__main__":
     assert stringify(value="12") == "12"
     assert stringify(value=15) == "15"
-    assert stringify(value=.5) == "0.5"
+    assert stringify(value=0.5) == "0.5"
     assert stringify(value=None) == "None"
